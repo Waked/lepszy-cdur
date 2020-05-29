5990e5e1fd60c1db293a1f45b8e59ffd36ce30b0
Waked <waked@incens.io>
2020-05-29 23:45:22 +0200

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+self.addEventListener('install', function(event) {
+  var CACHE_NAME = 'site-cache';
+  var urlsToCache = [
+    '/',
+    '/styles/pure-min.css',
+    '/script/main.js'
+  ];
+
+  self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
